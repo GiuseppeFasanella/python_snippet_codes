5a428d1ca2ce3e5dddb7e917182be764e21723bf
Giuseppe <I am Amerigo, you know my mail>
2015-05-09 17:04:40 +0200

    useful os listdir

---
+++ gauss.py
@@ -0,0 +1,5 @@
+import ROOT
+import math
+
+rand_gauss=ROOT.TRandom3()
+rand_gauss.Gaus(0.5,0.1)
--- os.py
+++ os.py
@@ -1,2 +1,7 @@
 import os
 os.system("command")
+
+#useful
+for file in os.listdir("my_dir"):
+    if file.endswith(".root"):
+        filename.append(file)
