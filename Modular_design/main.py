de32ca47f7b46ff3bbd2c7e69b3d6770c6db61a1
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-13 18:43:43 +0200

    This is the "main"

---
+++ Modular_design/main.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python                                                                                                                                                              
+
+from classes import *
+#This allows you to call your objects without typing classes.myObject                                                                                                           
+#On the other hand it's considered bad practice                                                                                                                                 
+print "this is my test"
+obj=myObject(2,3)
+print obj.n1
+print obj.n2
+print obj.sum
+
+#import classes                                                                                                                                                                 
+#obj=classes.myObject(2,3)                                                                                                                                                      
+#print obj.n1                                                                                                                                                                   
+#print obj.n2                                                                                                                                                                   
+#print obj.sum  
