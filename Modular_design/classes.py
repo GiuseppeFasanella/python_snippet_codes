473c693d445dd73a8b3f6333021ed4c857598c4d
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-13 18:44:44 +0200

    This is my class, imported in the main.py

---
+++ Modular_design/classes.py
@@ -0,0 +1,6 @@
+class myObject:
+    def __init__(self, n1, n2):#This is the constructor                                                                                                                         
+        self.n1 = n1
+        self.n2 = n2
+        self.sum = n1 + n2
+        self.regions = 'none'
