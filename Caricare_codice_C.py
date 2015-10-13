fca0f23eb1b545b7a9bdce40b5e7955ef244814c
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-10-13 15:05:16 +0200

    Create Caricare_codice_C.py

---
+++ Caricare_codice_C.py
@@ -0,0 +1,9 @@
+//Supponi di avere un certo codice in C
+//Lo compili bene
+//.L codice.C++ (in questo modo hai un .so
+
+nel python
+Load()
+
+Per chiamarlo 
+ROOT.coso (Importante questo ROOT. all'inizio)
