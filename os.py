653223cf8a57f58ee48da77f09b682d355895be9
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-10-20 11:47:03 +0200

    all files in dir, gestire argomenti

--- os.py
+++ os.py
@@ -5,3 +5,10 @@ os.system("command")
 for file in os.listdir("my_dir"):
     if file.endswith(".root"):
         filename.append(file)
+        
+profiles=['0','1']
+for profile in profiles:
+    #fai prima una stringa con tutti gli argomenti, poi la dai in pasto a system
+    command_to_run = "./script/fit.sh "+str("/outProfile_"+profile)
+    os.system(command_to_run)
+
