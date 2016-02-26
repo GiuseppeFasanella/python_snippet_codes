bdca7a9231acc2421502ec0416a868723073fb07
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-02-26 18:18:01 +0100

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,4 @@
+sum2=reduce(lambda x,y: x+y, map(lambda z,z*z, list))
+
+# Questa e' l'idea per fare le somme in quadratura
+# Ovviamente le parentesi, le virgole ecc, sono tutte sbagliate: questa e' solo l'idea
