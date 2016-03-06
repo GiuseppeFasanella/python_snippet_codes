946fc9f485532d3a6f08d278ab493a76f21b009e
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-03-06 14:59:33 +0100

    Update main.py

--- Modular_design/main.py
+++ Modular_design/main.py
@@ -9,6 +9,14 @@ print obj.n1
 print obj.n2
 print obj.sum
 
+#Se il .py che vuoi importare si trova in un'altra sottocartella, devi fare questo barbatrucco
+configfile = 'Closure_et_files/barrel_38T_scale.py'
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.expanduser(configfile)))
+from barrel_38T_scale import *
+
+
 #import classes                                                                                                                                                                 
 #obj=classes.myObject(2,3)                                                                                                                                                      
 #print obj.n1                                                                                                                                                                   
