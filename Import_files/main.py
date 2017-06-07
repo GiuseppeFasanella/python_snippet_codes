96dbd913863dea6ef2312ca85777c012948d9a70
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-06-07 16:12:25 +0200

    Update main.py

--- Import_files/main.py
+++ Import_files/main.py
@@ -1,3 +1,17 @@
 from HEEP_sel_60 import *
 
 my_abs(-2)
+
+##oppure, considerata una pratica migliore
+import HEEP_sel_60
+
+HEEP_sel_60.my_abs(-2)
+
+##Siccome poi e' seccante scrivere ogni volta tutto il nome del python
+##La pratica standard e'
+
+import HEEP_sel_60 as sel
+
+sel.my_abs(-2)
+
+#un po' come import numpy as np e simili
