83cd735182074d6be62d05f700c6d0578f5fcaab
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-07-13 09:04:31 +0200

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,2 +1,6 @@
 if("scale" in var_type): #if the substring scale is contained in var_type                                                                              
     
+    
+str = "this is string example....wow!!! this is really string"
+print str.replace("is", "was")
+print str.replace("is", "was", 3)
