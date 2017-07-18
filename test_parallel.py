a93a0b35aa573ae1612e2136c798a16be952f3b2
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-07-18 11:55:46 +0200

    Update test_parallel.py

--- test_parallel.py
+++ test_parallel.py
@@ -8,7 +8,11 @@ args_loop_list=[1,2]
 # Only create pool once                                                                                                                                               
 pool = Pool(processes=2)
 
+#func takes 2 arguments
 pool_result = [pool.apply_async(func, args=(arg1, 2)) for arg1 in args_loop_list]
+
+#func takes 1 argument: se non scrivi args=(arg1,) non capira' mai
+pool_result = [pool.apply_async(func, args=(arg1,)) for arg1 in args_loop_list]
 
 for i in pool_result:
     result = i.get()
