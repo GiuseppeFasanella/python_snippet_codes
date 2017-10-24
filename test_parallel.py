a03ad9959c05880641db08962f6706f129683f21
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-10-24 14:59:09 +0200

    Update test_parallel.py

--- test_parallel.py
+++ test_parallel.py
@@ -12,26 +12,13 @@ farms_ids=[247, 374, 546, 606, 626, 666, 667, 686, 740, 741, 742, 743, 744, 1044
  1367] #1226                                                                                                                                                          
 
 pool = Pool(processes=2)
+
+###case 1: function takes 1 argument
 pool_result = [pool.apply_async(Looper, args=(farm,)) for farm in farms_ids]
 for i in pool_result:
     result = i.get()
-##################################
-
-from multiprocessing import Pool
-
-def func(arg1, arg2):
-    print arg1 + arg2
-    return arg1 + arg2
-
-args_loop_list=[1,2]
-# Only create pool once                                                                                                                                               
-pool = Pool(processes=2)
 
 #func takes 2 arguments
-pool_result = [pool.apply_async(func, args=(arg1, 2)) for arg1 in args_loop_list]
-
-#func takes 1 argument: se non scrivi args=(arg1,) non capira' mai
-pool_result = [pool.apply_async(func, args=(arg1,)) for arg1 in args_loop_list]
-
+pool_result = [pool.apply_async(func, args=(arg1, arg2)) for arg1 in args_loop_list]
 for i in pool_result:
     result = i.get()
