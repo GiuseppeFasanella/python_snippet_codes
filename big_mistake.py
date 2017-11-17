08dd922473444e98856d41ad3c887926e3b64875
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-11-17 09:45:10 +0100

    Create big_mistake.py

---
+++ big_mistake.py
@@ -0,0 +1,18 @@
+def write_df_power_to_db(df_Power, fc_date, met_model, mdl_type, nation, zone_id, model_type_output,
+                         technology, provider, FConfig,
+                         df_Power_MN={},
+                         fc_date_to_local = True,
+                         value_cols = ['test'], debug=False):
+                         
+      value_cols.append('test2')
+      
+      
+      
+      
+e in un for sei fottuto
+
+
+Allora, devi fare value_cols= None (di default)
+
+e dentro if None:
+value_cols=['test','test2']
