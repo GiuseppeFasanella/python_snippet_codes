67d0d78ad38fd0be0da2f91570fe0942af0db1c1
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-11-20 15:10:45 +0100

    Update Solve_linear_system.py

--- Solve_linear_system.py
+++ Solve_linear_system.py
@@ -1,3 +1,4 @@
+######This is to find a solution for Ax=b, in the sense of the least squares
 import numpy as np
 from scipy import matrix
 from scipy.sparse.linalg import aslinearoperator
@@ -12,3 +13,28 @@ b = [6, 4]
 from scipy.sparse.linalg import lsqr
 x = lsqr(A, b)
 print(x[0])
+
+######Solve linear equation set #######
+#La condizione e' che la matrice sia non-degenere e quadrata
+import numpy as np
+a = np.array([[3,1], [1,2]])
+b = np.array([9,8])
+x = np.linalg.solve(a, b)
+print(x)
+
+print(np.allclose(np.dot(a, x), b))
+
+######Linear optimization problem #########
+#Problem is to minimize c*x
+#bound on the x vector
+#plus additional constraints: Ax<b
+c = [-1, 4]
+A = [[-3, 1], [1, 2]]
+b = [6, 4]
+#bounds on variables if they are not default (x_i > 0)                                                                                                                                  
+x0_bounds = (None, None)
+x1_bounds = (-3, None)
+from scipy.optimize import linprog
+res = linprog(c, A_ub=A, b_ub=b, bounds=(x0_bounds, x1_bounds),
+              options={"disp": True}) #disp is verbosity                                                                                                                                
+print(res)
