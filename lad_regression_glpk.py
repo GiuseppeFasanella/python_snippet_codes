ba7025cb4c75d97734336f80cda524470292b408
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-11-24 08:35:14 +0100

    Update and rename pyglpk.py to lad_regression_glpk.py

--- lad_regression_glpk.py
+++ lad_regression_glpk.py
@@ -1,3 +1,7 @@
+##M is a pandas dataframe
+##M contains competing models, last column of ones, the target variable (actuals)
+target='Actual'
+#########
 b=M[[target]]
 M=M.drop([target],axis=1)
 p=len(M.columns.values) ##length of available models + bias
@@ -72,43 +76,3 @@ else:
                                                                sol[2]
 
 ##PostProcess to be added: if <0 put 0, if > dispatch, put dispatch
-
-#############Simplified version
-
-import glpk            # Import the GLPK module
-
-lp = glpk.LPX()              # Create empty problem instance
-lp.name = 'optimization'     # Assign symbolic name to problem
-#lp.obj.maximize = True      # Set this as a maximization problem
-lp.obj.maximize = False      # Set this as a minimization problem
-
-
-#Define the incognitae
-lp.cols.add(3)         # Append three columns (incognitae) to this instance
-##Bounds on incognitae (if needed)
-for c in lp.cols:      # Iterate over all columns
-    c.name = 'x%d' % c.index # Name them x0, x1, x2,...
-    c.bounds = 0.0, None     # Set bound 0 <= xi < inf
-
-##Constraints
-lp.rows.add(3)         # Append three rows to this instance
-lp.rows[0].bounds = 100, 100.0  # Set equality p = 100
-lp.rows[1].bounds = 600, 600.0  # Set equality q = 600
-lp.rows[2].bounds = 300, 300.0  # Set equality r = 300
-
-##Define the problem 
-lp.obj[:] = [ 10.0, 6.0, 4.0 ]   # Set objective coefficients
-lp.matrix = [ 1.0, 1.0, 1.0,     # Set nonzero entries of the
-              10.0, 4.0, 5.0,     #   constraint matrix.  (In this
-              2.0, 2.0, 6.0 ]    #   case, all are non-zero.)
-
-##Solve
-lp.simplex()           # Solve this LP with the simplex method
-
-##Solution
-for c in lp.cols:
-    print(c.primal)
-#print(lp.cols.primal)
-#print 'Z = %g;' % lp.obj.value,  # Retrieve and print obj func value
-#print '; '.join('%s = %g' % (c.name, c.primal) for c in lp.cols)
-
