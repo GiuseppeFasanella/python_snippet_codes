96980deaa8dff7e9fc14cf1515ad2727924976fc
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2018-02-13 11:10:18 +0100

    Update try_catch.py

--- try_catch.py
+++ try_catch.py
@@ -9,3 +9,33 @@ try:
 except KeyError as e:
   logger.warning(met_model+' not available : skipping it')
   continue
+
+########## TRY-CATCH in PARALLEL ###################
+#Tu hai un programma parallelizzato su tanti core
+#In principio ogni thread puo' dare un suo errore diverso, di cui vuoi tener traccia.
+#La versione semplice sarebbe
+#try:                                                                                                                                                                                   
+#    for i in pool_result:                                                                                                                                                              
+#        result = i.get()                                                                                                                                                                                                                                                                                 
+#except Exception as err:                                                                     
+#    logger.error(err) ##Ma cosi' ogni errore ti manda una mail e fa spam
+
+###per ogni thread catcha l'errore e nel caso appendilo in una lista di messaggi, inviati solo alla fine
+error_messages=[]
+try:
+    for i in pool_result:                                                                                                                                                      
+        try:
+            result = i.get()
+        except Exception as err:
+            error_messages+=(err)
+except Exception as outer_err:
+    error_messages+=(outer_err)
+
+
+if len(error_messages)==0:
+    logger.info('**************************************** ')
+    logger.info('*         Succesfully finished         * ')
+    logger.info('**************************************** ')
+else:
+    logger.error(error_messages)
+
