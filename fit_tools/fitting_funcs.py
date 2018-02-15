93dc2fcb41dff76e155a00a6923e552bc25e284c
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2018-02-15 11:56:29 +0100

    Update fitting_funcs.py

--- fit_tools/fitting_funcs.py
+++ fit_tools/fitting_funcs.py
@@ -1,10 +1,15 @@
 import numpy as np
 
+###1D functions
 def exp(x, a, b, c):
     return a * np.exp(-b * x) + c
 
 def pol1(x, a, b):
     return a *x + b
+
+
+###2D functions
+###How to arrange the grid of independent variables --> see arrange_2D.py
 
 def plane(x, a, b, c):
     #x is the 2-d grid                                                                                                                                                                  
