fef8aaed9db987203e89f7b6099384315a739929
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2018-02-15 11:22:01 +0100

    Update fitting_funcs.py

--- fit_tools/fitting_funcs.py
+++ fit_tools/fitting_funcs.py
@@ -5,3 +5,9 @@ def exp(x, a, b, c):
 
 def pol1(x, a, b):
     return a *x + b
+
+def plane(x, a, b, c):
+    #x is the 2-d grid                                                                                                                                                                  
+    #x[0] is the "x"-coordinate                                                                                                                                                           
+    #x[1] is the "y"-coordinate                                                                                                                                                           
+    return a*x[0] + b*x[1] + c
