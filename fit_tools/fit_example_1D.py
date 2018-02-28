d3788a83a6950717ccf9c55ed9ec302556e888a1
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2018-02-28 10:38:32 +0100

    Update fit_example_1D.py

--- fit_tools/fit_example_1D.py
+++ fit_tools/fit_example_1D.py
@@ -20,6 +20,13 @@ popt, pcov = curve_fit(func, xdata, ydata, bounds=(0, [3., 1., 0.5]))
 plt.plot(xdata, func(xdata, *popt), 'g--',
          label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
 
+###How to force the curve to pass by the first and last point
+sigma = np.ones(len(x))
+sigma[[0, -1]] = 0.01 ##stai dando un peso 'sostanziale' al primo e all'ultimo punto
+#quindi sigma e' una penalty???! (comunque funziona)
+popt, pcov = curve_fit(model_func, x, y, p0=(0.1 ,1e-3, 0.1), sigma=sigma)
+
+
 plt.xlabel('x')
 plt.ylabel('y')
 plt.legend()
