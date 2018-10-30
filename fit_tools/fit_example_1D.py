2cb68e5d1645cf40c8a93665cc1b0cb4f5231b76
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2018-10-30 10:45:31 +0100

    Update fit_example_1D.py

--- fit_tools/fit_example_1D.py
+++ fit_tools/fit_example_1D.py
@@ -5,13 +5,16 @@ from scipy.optimize import curve_fit
 def func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
+### Generate some data with noise
 xdata = np.linspace(0, 4, 50)
 y = func(xdata, 2.5, 1.3, 0.5)
 np.random.seed(1729)
 y_noise = 0.2 * np.random.normal(size=xdata.size)
 ydata = y + y_noise
 plt.plot(xdata, ydata, 'b-', label='data')
+### Now y stores the data we want to fit
 
+### Make the fit
 popt, pcov = curve_fit(func, xdata, ydata)
 plt.plot(xdata, func(xdata, *popt), 'r-',
          label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
