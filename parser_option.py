793609be3ac0417018c6c6128144fcd869bddc70
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2018-11-26 12:24:43 +0100

    Update parser_option.py

--- parser_option.py
+++ parser_option.py
@@ -15,16 +15,15 @@ parser.add_argument("","--sideband",dest="sideband",default=False,action="store_
 parser.add_argument("","--addline",action="append",type="str",help="add lines to the plot file.root:color:linestyle:legend entry", default = [])
 #pass a list 
 parser.add_argument("-p","--point", dest='point',nargs='*', default=[0,1]) #'*' stands for 0 or more use 
-##pass a 'datetime'
-parser.add_argument("--train_st", default='',help="pass it as '20150101 00'")
-
-    
+##pass a 'datetime' (see how string--> datetime)
+parser.add_argument("--train_st", default='2015-01-01 00',help="pass it as '20150201 00'")
 #call it as
 #python prog.py --point 1 8 9 9
 #you'll have args.point as a list [1,8,9,9]
 
 
 args = parser.parse_args()
+args.train_st = datetime.strptime(args.train_st, '%Y-%m-%d %H')
 
 
 ####Examples###########                                                                                                                                               
