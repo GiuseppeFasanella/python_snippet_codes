09868d1855a399a958c0725e43ef2b2e8f0c2491
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2019-06-11 22:31:06 +0200

    Update string_list_to_int_list_commas.py

--- string_list_to_int_list_commas.py
+++ string_list_to_int_list_commas.py
@@ -1,14 +1,9 @@
-string='[5 1 2 3 7 8 6 4]'
-
 def from_string_to_list(string):
-    string = string.replace(' ', '')
+    string = string.replace(' ', ',')
     string = string.replace('[', '')
     string = string.replace(']', '')
-    q_list=[0]*len(string) 
-    for i in range(len(string)):
-        q_list[i]=int(string[i])
+    return list(map(int,string.split(',')))
 
-    return q_list
-    
-    
-q_list = [5,1,2,3......]
+a='[-1 -3 4 2]'
+a=from_string_to_list(a)
+
