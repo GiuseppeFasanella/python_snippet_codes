4ff67b96ba8bcead37680b336ff5978a0da19591
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2019-06-11 14:57:32 +0200

    Rename input_from_keyboard.py to input_from_keyboard_2.py

---
+++ input_from_keyboard_2.py
@@ -0,0 +1,11 @@
+'''
+7 
+1 2
+'''
+#take input  from keyboard
+print('Give me n')
+n = int(input()) #raw_input() in python2
+print('Give me the list')
+arr = list(map(int, input().split(' ')))
+print(arr)
+print('****')
