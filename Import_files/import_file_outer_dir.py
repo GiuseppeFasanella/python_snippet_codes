90254090fbdd1350cfe1381262ad3a1816204075
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2020-03-02 15:39:20 +0100

    Update import_file_outer_dir.py

--- Import_files/import_file_outer_dir.py
+++ Import_files/import_file_outer_dir.py
@@ -1,4 +1,10 @@
 import sys
-sys.path.append("M:/Dufenergy/Meteo/POWERANALYSIS/power_model/") 
 
+##supponiamo che bla.py sia nel path "M:/Dufenergy/Meteo/POWERANALYSIS/power_model/"
+sys.path.append("M:/Dufenergy/Meteo/POWERANALYSIS/power_model/") 
 from bla import bla bla
+
+# Puoi anche salire di un livello cosi'
+sys.path.append("..")
+# E ora hai "visibili" tutti i .py della cartella parent
+
